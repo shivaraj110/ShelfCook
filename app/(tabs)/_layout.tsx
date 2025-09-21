b727bea88d7a56422b40fa74e031722efa86a1f2
shivaraj110 <shivarajchandaragi9@gmail.com>
2025-09-21 22:53:49 +0530

    added tabs

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,5 +1,45 @@
-import { Stack } from "expo-router/stack";
+import { Tabs } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
 
-export default function Layout() {
-	return <Stack screenOptions={{ headerShown: false }} />;
+export default function TabLayout() {
+  return (
+    <Tabs screenOptions={{ headerShown: false }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="home" size={24} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="person" size={24} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="scan"
+        options={{
+          title: "Scan",
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="scan" size={24} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="explore"
+        options={{
+          title: "Explore",
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="compass" size={24} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
 }
---
+++ app/(tabs)/profile.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import { BackgroundWrapper } from "@/app/components/BackgroundWrapper";
+
+export default function Profile() {
+  return (
+    <BackgroundWrapper>
+      <View className="flex-1 justify-center items-center">
+        <Text className="text-white text-xl">Profile</Text>
+      </View>
+    </BackgroundWrapper>
+  );
+}
